457c2af7a527e50b17e5445afb6ee8393194f356
Alexander Kadyrov <alexander-kad@yandex.ru>
2020-01-26 01:21:40 +0300

    add ConfigurationService.swift

---
+++ Flight2/Sources/Classes/Controllers/Services/Configuration/ConfigurationService.swift
@@ -0,0 +1,18 @@
+import ReactiveSwift
+import Foundation
+import NWClient
+
+final class ConfigurationService: NWService {
+    
+    // MARK: - Network Method
+    
+    public static func fetchConfiguration(source: String) -> SignalProducer<Configuration, Error> {
+        let request = NWRequest(source: source)
+        return make(request: request).flatMap(.latest, { data -> SignalProducer<Configuration, Error> in
+            if let value = JSONDecoder.decode(Configuration.self, from: data) {
+                return SignalProducer(value: value)
+            }
+            return SignalProducer(error: NSError.errorBadRequest)
+        })
+    }
+}
