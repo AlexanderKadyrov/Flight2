4de56f2e53b16b64c346d2a32b7bc3a1ec7e61c9
Alexander Kadyrov <alexander-kad@yandex.ru>
2020-01-26 01:12:44 +0300

    add MoveService.swift

---
+++ Flight2/Sources/Classes/Controllers/Services/Move/MoveService.swift
@@ -0,0 +1,18 @@
+import ReactiveSwift
+import Foundation
+import NWClient
+
+final class MoveService: NWService {
+    
+    // MARK: - Network Method
+    
+    public static func fetchMoveList(source: String) -> SignalProducer<[Move], Error> {
+        let request = NWRequest(source: source)
+        return make(request: request).flatMap(.latest, { data -> SignalProducer<[Move], Error> in
+            if let list = JSONDecoder.decode([Move].self, from: data) {
+                return SignalProducer(value: list)
+            }
+            return SignalProducer(error: NSError.errorBadRequest)
+        })
+    }
+}
