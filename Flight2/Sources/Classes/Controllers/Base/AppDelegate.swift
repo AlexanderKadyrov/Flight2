ad76a67889e77a51eea1b2f9a60a3b249701009a
Alexander Kadyrov <alexander-kad@yandex.ru>
2020-01-26 12:06:52 +0300

    work with AppDelegate.swift

--- Flight2/Sources/Classes/Controllers/Base/AppDelegate.swift
+++ Flight2/Sources/Classes/Controllers/Base/AppDelegate.swift
@@ -2,7 +2,19 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    // MARK: - Properties
+    
+    public var window: UIWindow?
+    
+    // MARK: - Application
+    
     public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let vc = FlightListViewController()
+        let nv = NavigationController(vc, color: .colorOnyx)
+        window?.rootViewController = nv
+        window?.makeKeyAndVisible()
+        window?.tintColor = .white
         return true
     }
 }
