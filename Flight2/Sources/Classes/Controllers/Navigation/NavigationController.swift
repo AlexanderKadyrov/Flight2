05736e4ac5cb2d07f17e590cb3b179998310992c
Alexander Kadyrov <alexander-kad@yandex.ru>
2020-01-26 12:02:17 +0300

    add NavigationController.swift

---
+++ Flight2/Sources/Classes/Controllers/Navigation/NavigationController.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class NavigationController: UINavigationController {
+    
+    // MARK: - Initialization
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    public init(_ rootViewController: UIViewController, color: UIColor = .white) {
+        super.init(rootViewController: rootViewController)
+        self.navigationBar.backgroundColor = color
+        self.navigationBar.isTranslucent = false
+    }
+}
