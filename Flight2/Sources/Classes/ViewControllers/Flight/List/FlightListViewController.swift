37018987942dbbb22b53d5e4da48e0b1660f4fb4
Alexander Kadyrov <alexander-kad@yandex.ru>
2020-01-26 13:37:21 +0300

    work with FlightListViewController.swift

--- Flight2/Sources/Classes/ViewControllers/Flight/List/FlightListViewController.swift
+++ Flight2/Sources/Classes/ViewControllers/Flight/List/FlightListViewController.swift
@@ -1,8 +1,16 @@
+import TabloidView
+import SnapKit
 import UIKit
 
 final class FlightListViewController: BaseViewController {
     
     // MARK: - Properties
+    
+    public lazy var tabloidView: TabloidView = {
+        let view = TabloidView(viewModel: viewModel.tabloidViewModel)
+        view.backgroundColor = .clear
+        return view
+    }()
     
     public var viewModel: FlightListViewModel!
     
@@ -10,10 +18,18 @@ final class FlightListViewController: BaseViewController {
     
     public override func viewDidLoad() {
         super.viewDidLoad()
+        makeTableView()
         makeToolbar()
     }
     
     // MARK: - Make
+    
+    private func makeTableView() {
+        view.addSubview(tabloidView)
+        tabloidView.snp.makeConstraints({ maker in
+            maker.edges.equalToSuperview()
+        })
+    }
     
     private func makeToolbar() {
         setBarTitle("flight_navigation_bar_title".localized() + " " + "App")
