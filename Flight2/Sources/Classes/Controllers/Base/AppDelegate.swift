741701fef1deb7720394191ea8aaa43bf25b59f2
Alexander Kadyrov <alexander-kad@yandex.ru>
2020-01-27 01:25:48 +0300

    work with AppDelegate.swift

--- Flight2/Sources/Classes/Controllers/Base/AppDelegate.swift
+++ Flight2/Sources/Classes/Controllers/Base/AppDelegate.swift
@@ -11,12 +11,19 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     
     public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
+        window?.launch()
+        return true
+    }
+}
+
+fileprivate extension UIWindow {
+    func launch() {
         let vc = FlightListViewController()
         vc.viewModel = FlightListViewModel()
+        vc.router = FlightListRouter()
         let nv = NavigationController(vc, color: .colorBackground)
-        window?.rootViewController = nv
-        window?.makeKeyAndVisible()
-        window?.tintColor = .white
-        return true
+        rootViewController = nv
+        makeKeyAndVisible()
+        tintColor = .white
     }
 }
