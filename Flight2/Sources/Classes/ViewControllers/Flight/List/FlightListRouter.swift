7f19f7b80093bd6ff9a06363bd08c07c561d49fe
Alexander Kadyrov <alexander-kad@yandex.ru>
2020-01-27 01:31:00 +0300

    work with FlightListRouter.swift

--- Flight2/Sources/Classes/ViewControllers/Flight/List/FlightListRouter.swift
+++ Flight2/Sources/Classes/ViewControllers/Flight/List/FlightListRouter.swift
@@ -5,6 +5,14 @@ final class FlightListRouter {
     // MARK: - Perform
     
     public func perform(_ value: FlightProtocol, from source: UIViewController) {
-        
+        if value is Notice {
+            let vc = NoticeDetailViewController()
+            vc.viewModel = NoticeDetailViewModel()
+            source.navigationController?.pushViewController(vc, animated: true)
+        } else if value is Event {
+            
+        } else if value is Move {
+            
+        }
     }
 }
