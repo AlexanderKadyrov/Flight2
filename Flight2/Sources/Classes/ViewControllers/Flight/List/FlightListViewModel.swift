e1eb4bf99b9aa18f379b1e18b995fe3e7dbe3d8a
Alexander Kadyrov <alexander-kad@yandex.ru>
2020-01-26 13:08:19 +0300

    work with FlightListViewModel.swift

--- Flight2/Sources/Classes/ViewControllers/Flight/List/FlightListViewModel.swift
+++ Flight2/Sources/Classes/ViewControllers/Flight/List/FlightListViewModel.swift
@@ -1,5 +1,18 @@
+import ReactiveSwift
+import TabloidView
 import Foundation
+
+fileprivate let source = "https://github.com/AlexanderKadyrov/JSONs/blob/master/Flight/seeder_configuration.json"
 
 final class FlightListViewModel {
     
+    // MARK: - Properties
+    
+    public var actionFetchFlightList = Action<(), [FlightProtocol], Error> { input in
+        return FlightService.fetchFlightList(source: source)
+    }
+    
+    public let tabloidViewModel = TabloidViewModel(tabloidStyle: .plain,
+                                                   separatorStyle: .none,
+                                                   cellIdentifiers: ["FlightListTableViewCell"])
 }
