c8563b3004240a9a6eaa22c198216f01b57e29d1
Alexander Kadyrov <alexander-kad@yandex.ru>
2020-01-26 15:43:49 +0300

    fix FlightListViewController.swift

--- Flight2/Sources/Classes/ViewControllers/Flight/List/FlightListViewController.swift
+++ Flight2/Sources/Classes/ViewControllers/Flight/List/FlightListViewController.swift
@@ -8,8 +8,7 @@ final class FlightListViewController: BaseViewController {
     
     public lazy var tabloidView: TabloidView = {
         let view = TabloidView(viewModel: viewModel.tabloidViewModel)
-        view.tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: 12))
-        view.backgroundColor = .clear
+        view.tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: 0, height: 12))
         return view
     }()
     
